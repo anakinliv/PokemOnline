27710962b9c772ed38bee3b7d0c7b59904d8b6a2
anakinliv <anakinliv@126.com>
2011-12-22 17:04:03 +0800

    Update pokemon.sql

--- pokemon.sql
+++ pokemon.sql
@@ -271,6 +271,7 @@ create table skill_effect
 (
    skillid              int not null,
    effectid             bigint not null,
+   accuracy             int not null,
    primary key (skillid, effectid)
 );
 
